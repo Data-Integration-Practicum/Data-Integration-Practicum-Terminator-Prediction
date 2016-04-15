e904df215e8f17e22f763c3acd97590e6418368c
charlotte <cdarby@alumni.cmu.edu>
2016-04-15 18:49:54 -0400

    data for salt genes and dec tree code
    char

--- pipeline/getpssm.py
+++ pipeline/getpssm.py
@@ -3,12 +3,13 @@ import string
 with open(sys.argv[1]) as F:
     contents = F.readlines();
 name = sys.argv[2]
-nmotifs = sys.argv[3]
+nmotifs = int(sys.argv[3])
 ln = 0
 for m in xrange(nmotifs):
-    while not string.startswith(contents[ln],"log-odds matrix:"):
+    while not contents[ln].startswith("log-odds matrix:"):
         ln += 1
     with open(name+"-"+str(m), 'w') as F:
+        ln += 1
         while contents[ln][0] != "-":
             F.write(contents[ln])
             ln += 1
--- pipeline/splitsegments1.py
+++ pipeline/splitsegments1.py
@@ -11,22 +11,22 @@ with open("trimmedseqs.fa",'w') as out:
         seq = contents[j].strip()
         while True:
             j +=1
-       	if j == len(contents) or len(contents[j]) == 0:
-        	   break
-       	if contents[j][0] != ">":
-        	   seq += contents[j].strip()
-       	else:
-        	   break
+            if j == len(contents) or len(contents[j]) == 0:
+                break
+            if contents[j][0] != ">":
+                seq += contents[j].strip()
+       	    else:
+                break
         if "N" in seq:
             continue
-    if len(seq) > 400:
-        out.write(head)
-        out.write(seq[-400:])
-        out.write("\n")	
-        for i in xrange(8):
-            allFiles[i].write(head)
-            if i == 0:
-                allFiles[i].write(seq[-50:])
-            else: 
-                allFiles[i].write(seq[-(i+1)*50:-i*50])
-            allFiles[i].write('\n')
+        if len(seq) > 400:
+            out.write(head)
+            out.write(seq[-400:])
+            out.write("\n")	
+            for i in xrange(8):
+                allFiles[i].write(head)
+                if i == 0:
+                    allFiles[i].write(seq[-50:])
+                else: 
+                    allFiles[i].write(seq[-(i+1)*50:-i*50])
+                allFiles[i].write('\n')
