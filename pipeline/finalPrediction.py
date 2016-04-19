3509df35fdc06a50847e845e7c97b3213f6ca947
Xupeng Tong <xupengtong@Xupengs-MacBook-Pro.local>
2016-04-18 23:41:41 -0400

    near complete

---
+++ pipeline/finalPrediction.py
@@ -0,0 +1,42 @@
+from Bio import SeqIO
+from trainModel import Model
+import pandas as pd
+import pickle
+import sys
+
+# Classifier's file name
+try:
+    model = pickle.load(open('./classifiers/' + sys.argv[1], "r"))
+except IOError:
+    print "The classifier of " + sys.argv[1] \
+    + "is not existed under the folder ./classifiers."
+
+# The file name of the genome to be predicted
+genomeFile = sys.argv[2]
+
+columns = ['ID', 'Offset', 'Nearby Sequence']
+results = pd.DataFrame(columns=columns)
+
+for seq_record in SeqIO.parse(genomeFile, "fasta"):
+    i = 0
+    sequence = seq_record.seq
+    while i + 400 < len(sequence):
+        if i % 16000 == 0:
+            print seq_record.id + " has been processed for " + str(i) + " bp"
+
+        currSeq = str(sequence[i:i+400])
+        prediction = 0
+        
+        if "N" not in currSeq:            
+            model.predict(currSeq)
+            
+            if prediction == 1:
+                ## add seq_record.id, offset(i+300), nearby sequence(str(seq_record[i+300:i+400])), to results dataframe
+                curDF = pd.DataFrame([[seq_record.id, i+300,str(sequence[i+300:i+400])]], columns=columns)
+                results.append(curDF)
+                print "TTS found in" + sequence[i+300:i+400]
+                
+        i = i + 50
+
+# Output the prediction result
+results.to_csv('TTS.csv')
--- pipeline/trainModel.py
+++ pipeline/trainModel.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.externals import joblib
+import pickle
 
 from featureHelpers import *
 
@@ -7,66 +7,74 @@ import numpy as np
 import random
 import sys
 
-class PredictModel():
-	rf = RandomForestClassifier(criterion="entropy", n_estimators = 300, max_depth = 100)
 
-	def __init__(self, GO):
-		self.GO = GO
-
-		# Get PSSMs
-		allPSSMFiles = ["./" + self.GO + "/segment" + str(i) + "-" + str(j) \
-						for i in xrange(1,9) for j in xrange(4)]
-
-		self.allPSSMs = [readMatrix(f) for f in allPSSMFiles]
-
-		# GCFeat returns a list of all 8
-		self.PosFeatures = [GCFeat(seq) for seq in pos if seq[0] != '>']
-		self.NegFeatures = [GCFeat(seq) for seq in neg if seq[0] != '>']
-
-		self.__trainModel()
-
-	# Calculate the feature of the newly passed in sequence		
-	def getPrediction(self, seq):
-		feat = GCFeat(seq)
-
-		for M in self.allPSSMs:
-			feat.append(computeMaxScore(M, seq))
-
-		return rf.predict(feat)[0]
-
-	# Run this before get prediction
-	def __trainModel(self):
-		with open("negative.fa") as F:
-			neg = F.readlines()
-			nneg = len(neg)/2
-
-		with open("./" + self.GO + "/trimmedseqs.fa") as F:
-		    pos=F.readlines()
-
-		npos= len(pos)/2
-		S = random.sample(range(nneg),npos)
-		neg = [neg[i*2+1] for i in S]
-		nmotifs = 4 #sys.argv[1]
-
-		for M in self.allPSSMs:
-		    for i in xrange(npos): 
-		        PosFeatures[i].append(computeMaxScore(M,pos[i*2+1]))
-		        NegFeatures[i].append(computeMaxScore(M,neg[i]))
-		print "Train features calculated"
-
-		#X: each sample is a row, each column is a feature
-		X = np.array(list(PosFeatures) + list(random.sample(NegFeatures,npos)), dtype=np.float)
-		#y: labels
-		y = np.array([1 for x in xrange(npos)] + [0 for x in xrange(npos)])
-
-		combined = zip(X, y)
-		random.shuffle(combined)
-		X, y = zip(*combined)
-
-		rf.fit(X, y)
+rf = RandomForestClassifier(criterion="entropy", n_estimators = 300, max_depth = 100)
 
 GO = sys.argv[1]
 
-model = PredictModel(GO)
+#####################
+# Get Train samples #
+#####################
+with open("negative.fa") as F:
+	neg = F.readlines()
+	nneg = len(neg)/2
 
-joblib.dump(model, 'classifier' + '_' + GO)
+with open(GO + "/trimmedseqs.fa") as F:
+    pos=F.readlines()
+
+npos= len(pos)/2
+S = random.sample(range(nneg),npos)
+neg = [neg[i*2+1] for i in S]
+nmotifs = 4 #sys.argv[1]
+
+#############
+# Get PSSMs #
+#############
+
+allPSSMFiles = [GO + "/" + "segment" + str(i) + "-" + str(j) \
+			   for i in xrange(1,9) for j in xrange(nmotifs)]
+allPSSMs = [readMatrix(f) for f in allPSSMFiles]
+
+# GCFeat returns a list of all 8
+PosFeatures = [GCFeat(seq) for seq in pos if seq[0] != '>']
+NegFeatures = [GCFeat(seq) for seq in neg if seq[0] != '>']
+
+
+###############
+# Train model #
+###############
+for M in allPSSMs:
+    for i in xrange(npos): 
+        PosFeatures[i].append(computeMaxScore(M,pos[i*2+1]))
+        NegFeatures[i].append(computeMaxScore(M,neg[i]))
+print "Train features calculated"
+
+#X: each sample is a row, each column is a feature
+X = np.array(list(PosFeatures) + list(random.sample(NegFeatures,npos)), dtype=np.float)
+#y: labels
+y = np.array([1 for x in xrange(npos)] + [0 for x in xrange(npos)])
+
+combined = zip(X, y)
+random.shuffle(combined)
+X, y = zip(*combined)
+
+rf.fit(X, y)
+
+class Model():
+	# Calculate the feature of the newly passed in sequence
+	def __init__(self):
+		self.rf = rf
+		self.PSSMs = allPSSMs
+
+	def predict(self, seq):
+		feat = GCFeat(seq)
+
+		for M in self.PSSMs:
+			feat.append(computeMaxScore(M, seq))
+
+		return self.rf.predict(np.array(feat).reshape(1, -1))[0]
+
+
+model = Model()
+
+pickle.dump(model, open('./classifiers/' + GO, "w"))
