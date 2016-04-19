22fa0276a65a3988f091d0aacd6ae9f7d248f548
Xupeng Tong <xupengtong@Xupengs-MBP.wv.cc.cmu.edu>
2016-04-19 16:47:48 -0400

    fixed finalprediction

--- pipeline/finalPrediction.py
+++ pipeline/finalPrediction.py
@@ -6,18 +6,22 @@ import pandas as pd
 import pickle
 import sys
 
-# This is file takes a GO term and a file name of genome to be tested
-# as imput, and gives the final prediction
-
+"""
+    This is file takes a GO term and a file name of genome to be tested
+    as imput, and gives the final prediction
+"""
 # Classifier's file name
+GO = sys.argv[1]
+# The file name of the genome to be predicted (should be .fa file)
+genomeFile = sys.argv[2]
+
+fileName = genomeFile.split('/')[-1][:-3]
+
 try:
-    model = pickle.load(open('./classifiers/' + sys.argv[1], "r"))
+    model = pickle.load(open('./classifiers/' + GO, "r"))
 except IOError:
     print "The classifier of " + sys.argv[1] \
     + "is not existed under the folder ./classifiers."
-
-# The file name of the genome to be predicted
-genomeFile = sys.argv[2]
 
 columns = ['ID', 'Offset', 'Nearby Sequence']
 results = pd.DataFrame(columns=columns)
@@ -27,7 +31,7 @@ for seq_record in SeqIO.parse(genomeFile, "fasta"):
     count = 0
     sequence = seq_record.seq
     while i + 400 < len(sequence):
-        
+
         # Print every 16,000 bps are processed
         if i % 16000 == 0:
             print seq_record.id + " has been processed for " + str(i) + " bp"
@@ -42,11 +46,12 @@ for seq_record in SeqIO.parse(genomeFile, "fasta"):
             
             if prediction == 1:
                 ## add seq_record.id, offset(i+300), nearby sequence(str(seq_record[i+300:i+400])), to results dataframe
-                curDF = pd.DataFrame([[seq_record.id, i+300,str(sequence[i+300:i+400])]], columns=columns)
-                results.append(curDF)
+                curDF = [seq_record.id, i+300, str(sequence[i+300:i+400])]
+                results.loc[len(results)] = curDF
+
                 count = count + 1
                 
         i = i + 50
 
 # Output the prediction result
-results.to_csv('TTS.csv')
+results.to_csv('./results/' + GO + '_TTS_' + fileName + '.csv')
